c7919911fe4aa4a38f21209b670a4424410ce18d
汪林林 <wll_604@163.com>
2025-03-04 14:48:39 +0800

    修复

--- vite.preload.config.ts
+++ vite.preload.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config
 export default defineConfig({
   build: {
-    outDir: '.vite/preload',
+    outDir: '.vite/main/preload',
     emptyOutDir: false,
   },
 });
